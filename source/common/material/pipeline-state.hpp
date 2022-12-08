c97614471489f42ce35527524a52ecbd68e59125
nouran <nouran2000.foda@gmail.com>
2022-12-08 10:12:14 +0200

    pipiline-requirement

--- source/common/material/pipeline-state.hpp
+++ source/common/material/pipeline-state.hpp
@@ -42,6 +42,51 @@ namespace our {
         // For example, if faceCulling.enabled is true, you should call glEnable(GL_CULL_FACE), otherwise, you should call glDisable(GL_CULL_FACE)
         void setup() const {
             //TODO: (Req 4) Write this function
+            if (faceCulling.enabled) {
+                glEnable(GL_CULL_FACE);
+
+                // glCullFace is used to specify which face will not be drawn
+                glCullFace(faceCulling.culledFace);
+
+                // glFrontFace Specifies the orientation of front-facing polygons
+                glFrontFace(faceCulling.frontFace);
+            }
+            else {
+                glDisable(GL_CULL_FACE);
+            }
+
+            if (depthTesting.enabled) {
+                glEnable(GL_DEPTH_TEST);
+
+                // glDepthFunc Specifies the depth comparison function that determines which fragments are drawn
+                // e.g  GL_LESS will draw a fragment if its depth value is less than the depth value stored in the depth buffer
+                glDepthFunc(depthTesting.function);
+            }
+            else {
+                glDisable(GL_DEPTH_TEST);
+            }
+
+            if (blending.enabled) {
+                glEnable(GL_BLEND);
+                // glBlendEquation Specifies the blending equation that will be used to combine the source and destination colors
+                glBlendEquation(blending.equation);
+
+                // glBlendFunc Specifies the source and destination factors used in the blending equation
+                // e.g. GL_SRC_ALPHA and GL_ONE_MINUS_SRC_ALPHA will blend the source and destination colors using the alpha value of the source color
+                glBlendFunc(blending.sourceFactor, blending.destinationFactor);
+
+                // glBlendColor Specifies the constant color used in the blending equation
+                // e.g. if the blending equation is GL_FUNC_ADD, the final color will be (sourceColor * sourceFactor) + (destinationColor * destinationFactor) + constantColor
+                glBlendColor(blending.constantColor.r, blending.constantColor.g, blending.constantColor.b, blending.constantColor.a);
+            }
+            else {
+                glDisable(GL_BLEND);
+            }
+            // glColorMask Specifies whether the red, green, blue, and alpha components are written into the color buffer
+            glColorMask(colorMask.r, colorMask.g, colorMask.b, colorMask.a);
+            
+            // glDepthMask Specifies whether the depth buffer is enabled for writing
+            glDepthMask(depthMask);
         }
 
         // Given a json object, this function deserializes a PipelineState structure
