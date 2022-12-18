6d6e9feb7fcabff3cf19a3230f86bfd92165dc1e
Mohamed Mostafa <mohamedmostafa1632001@gmail.com>
2022-12-18 02:00:30 +0200

    Fixed Application and Post Process

--- source/common/material/material.cpp
+++ source/common/material/material.cpp
@@ -46,8 +46,8 @@ namespace our {
         TintedMaterial::setup();
         shader->set("alphaThreshold", alphaThreshold);
         glActiveTexture(GL_TEXTURE0);
-        texture->bind();
-        sampler->bind(0);
+        if(texture){texture->bind();}
+        if(sampler){sampler->bind(0);}
         shader->set("tex", 0);
         
     }
--- source/common/systems/forward-renderer.cpp
+++ source/common/systems/forward-renderer.cpp
@@ -154,8 +154,7 @@ namespace our {
         std::sort(transparentCommands.begin(), transparentCommands.end(), [cameraForward](const RenderCommand& first, const RenderCommand& second){
             //TODO: (Req 9) Finish this function
             // HINT: the following return should return true "first" should be drawn before "second".
-            
-            return (first.center.z>second.center.z);
+            return (first.center.z<second.center.z);
         });
 
         //TODO: (Req 9) Get the camera ViewProjection matrix and store it in VP
