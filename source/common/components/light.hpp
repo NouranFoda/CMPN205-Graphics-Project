ed42151b7465569941afdb987d8f1658045de36b
Dina Ibrahim <dina.ibrahim.1@outlook.com>
2022-12-31 11:49:09 +0200

    added light class

---
+++ source/common/components/light.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "../ecs/component.hpp"
+#include <glm/glm.hpp>
+#include <glm/gtx/hash.hpp>
+
+namespace our
+{
+
+    @ @-10, 6 + 12, 9 @ @ namespace our{POINT, SPOT};
+    // Since we may want to store colors in bytes instead of floats for efficiency,
+    // we are creating our own 32-bit R8G8B8A8 Color data type with the default GLM precision
+    typedef glm::vec<4, glm::uint8, glm::defaultp> Color;
+    // This component adds lighting ability to the owing entity.
+    class LightComponent : public Component
+    {
+    public:
+        LightType type;        // The current type of light, Possible values are Directional, Point and Spot.
+        Color color;           // The light color
+        glm::vec2 cone_angles; // define the angle of a spot light’s cone (Spot light only).
+
+        // The ID of this component type is "Mesh Renderer"
+        static std::string getID() { return "Light"; }
+
+        // Reads light properties by the names given in the json object
+        void deserialize(const nlohmann::json &data) override;
+    };
+
+}
